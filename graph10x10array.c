573dba4fab9bd36086a81a9346e39bf56ce8b89e
Aash-Sasha <physicswithaash@gmail.com>
2016-11-30 19:23:52 +0530

    Create graph10x10array.c

---
+++ graph10x10array.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{  
+	int g[10][10], b, a, n, p, x, y;
+	//make value of all element 0 by default
+	for (a = 1; a <= 10; a = a + 1)
+	{
+		for (b = 1; b <= 10; b = b + 1)
+		{
+			g[a][b] = 0;
+		}
+	}
+
+	//welcome screen
+	printf("-=AaSh=-\n10x10 graph\n");
+
+	//ask number of point user want to plot
+	printf("How many points you want to plot ?\n");
+	scanf_s("%d", &n);
+
+	//getting input to know which point to be plotted
+	for (p = 1; p <= n; p = p + 1)
+	{
+		printf("\nINPUT x and y of %d point\n", p);
+		scanf_s("%d %d",&x,&y);
+		g[y][x] = 1;
+	}
+
+	//print the graph
+	for (a = 10; a >=1; a = a - 1)
+	{
+		if (a > 9)
+			printf("\n%d| ", a);
+		else
+			printf("\n%d | ", a);
+		for (b = 1; b <= 10; b = b + 1)
+		{
+			printf("%d ", g[a][b]);
+		}
+	}
+	printf("\n-----------------------\n    1 2 3 4 5 6 7 8 9 10\n");
+	_getch();
+}
